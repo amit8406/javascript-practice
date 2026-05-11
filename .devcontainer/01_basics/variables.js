6b33ed5af1398db65ec019d9ccd4520711cf5582
Amit Tiparadi <amit.tiparadi1@gmail.com>
2026-05-11 18:17:25 +0530

    variables.js file updated

--- .devcontainer/01_basics/variables.js
+++ .devcontainer/01_basics/variables.js
@@ -1,11 +1,20 @@
 const accountId = 3492340
-let accountEmail= "amit.test@yopmail.com"
+const accountType = "SAVINGS"
+let accountEmail= "amit.test2@yopmail.com"
 var accountPassword= "abcd"
-accountCity= "SOLAPUR"
-//accountId= 899
+const accountCity= "SOLAPUR"
+//const accountId= 899
+let Fees = 450000
+//accountEmail= "sagar2@yopmail.com"
+//const accountCity= "PUNE"
 
-accountEmail= "sagar@yopmail.com"
-accountCity= "PUNE"
+console.table({accountId, accountType, accountEmail, accountPassword, accountCity, Fees})
 
-console.table({accountId, accountEmail, accountPassword, accountCity})
+/*
+Const and let should be used to declare variables in JavaScript. 
+Const is used for variables that should not be reassigned, while let is used for variables that can be reassigned. This helps to prevent accidental reassignments and makes your code more predictable and easier to debug.
+*/
 
+/*
+Don't ever use var, it is a legacy way of declaring variables. It has some weird behavior and can lead to bugs in your code. Always use const or let instead.
+*/
