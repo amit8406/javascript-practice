016fbaa6acd624d66e43a064a990be6d215b99f7
amit8406 <amit.tiparadi1@gmail.com>
2026-05-18 10:42:10 +0000

    Commited small changes

---
+++ .devcontainer/01_basics/conversionOp.js
@@ -0,0 +1,9 @@
+let str1 = "Hello"
+let str2 = "Javascript"
+
+str3 = str1 + str2
+console.log(str1 + str2)
+
+console.log("3" + 5 + 7 + "67")
+
+console.log((5+8) + (4 * 3))
---
+++ .devcontainer/01_basics/dataType.js
@@ -0,0 +1,17 @@
+let someNumber = "99324vb";
+
+console.log(someNumber)
+console.log(typeof(someNumber))
+let score = "1"; 
+
+
+let valueInNum = String(score)
+console.log(valueInNum)
+
+
+let value = "Aims"
+
+console.log(typeof(value))
+console.log(typeof(score))
+
+co
