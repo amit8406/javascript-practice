89ad4d7ae1dbb59adad8df574f49af706e434689
amit8406 <amit.tiparadi1@gmail.com>
2026-05-18 11:55:01 +0000

    commited changes

---
+++ .devcontainer/dataTypesSummary.js
@@ -0,0 +1,49 @@
+const score = "1500"
+const scr = 100.50
+const isLogged = true
+
+const temp =null
+console.log(Number(scr))
+const id = Symbol('124')
+const id2 = Symbol('124')
+let user;
+console.log(isLogged)
+console.log(Number(score))
+console.log(temp)
+console.log(user)
+
+console.log(id===id2);
+
+const movies =["Dhurnadhar", "War", "Criminal Justice"]
+
+let myObj = {
+    name: "Sanjeev",
+    age: 25,
+}
+
+const myFun = function(){
+    console.log("Hello Chcha ji");
+}
+
+console.log(typeof(isLogged))
+console.log(typeof(movies))
+console.log(typeof(temp))
+console.log(typeof(score))
+console.log(typeof(scr))
+console.log(typeof(id))
+console.log(typeof(id2))
+console.log(typeof(myFun))
+console.log(typeof(myObj))
+
+/*
+ReturnTypes:
+isLogged: boolean
+movies: object
+temp: object
+score: string
+scr: number
+id: symbol
+id2: symbol
+myFun: function
+
+*/
