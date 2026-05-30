effca5321f47e28e19dffde68931a356eca08da0
amit8406 <amit.tiparadi1@gmail.com>
2026-05-30 14:57:50 +0000

    commited code

---
+++ .devcontainer/01_basics/math.js
@@ -0,0 +1,16 @@
+
+const otherNum = new Number(2045.6799)
+console.log(otherNum)
+console.log(otherNum.valueOf())
+console.log(otherNum.toString().length)
+console.log(otherNum.toExponential())
+console.log(otherNum.toFixed(2))
+console.log(otherNum.toPrecision(6))
+console.log(Math.random())
+console.log((Math.random() * 10) + 1)
+
+
+
+const min =10
+const max =20
+console.log(Math.floor((Math.random() * (max-min +1) )+ min))
