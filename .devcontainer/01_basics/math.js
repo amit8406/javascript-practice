0ec4e9341041d80cce7d575577d51e2bb14473fe
amit8406 <amit.tiparadi1@gmail.com>
2026-06-01 10:30:40 +0000

    code commited for math.js

--- .devcontainer/01_basics/math.js
+++ .devcontainer/01_basics/math.js
@@ -1,16 +1,41 @@
 
-const otherNum = new Number(2045.6799)
-console.log(otherNum)
-console.log(otherNum.valueOf())
-console.log(otherNum.toString().length)
-console.log(otherNum.toExponential())
-console.log(otherNum.toFixed(2))
-console.log(otherNum.toPrecision(6))
-console.log(Math.random())
-console.log((Math.random() * 10) + 1)
+// const otherNum = new Number(2045.6799)
+// console.log(otherNum)
+// console.log(otherNum.valueOf())
+// console.log(otherNum.toString().length)
+// console.log(otherNum.toExponential())
+// console.log(otherNum.toFixed(2))
+// console.log(otherNum.toPrecision(6))
+// console.log(Math.random())
+// console.log((Math.random() * 10) + 1)
 
 
 
-const min =10
-const max =20
-console.log(Math.floor((Math.random() * (max-min +1) )+ min))
+// const min =10
+// const max =20
+// console.log(Math.floor((Math.random() * (max-min +1) )+ min))
+
+// const marks = 1000.67
+// console.log(marks.toString().length)
+// console.log(marks.toFixed(0))
+// console.log(marks.toLocaleString('en-IN'))
+// console.log(marks.toPrecision(3))
+// console.log(marks.toExponential(0))
+
+// const num= 10800
+
+//++++++++++++++++++++++++++++++++++++  Maths  ++++++++++++++++++++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-5))
+// console.log(Math.ceil(4.2))
+// console.log(Math.floor(4.8))
+// console.log(Math.round(4.5))
+// console.log(Math.max(1, 5, 3, 9, 2))
+// console.log(Math.min(1, 5, 3, 9, 2))
+// console.log(Math.pow(2, 3))
+// console.log(Math.sqrt(16))
+console.log(Math.random()) //return a random number between 0 and 1
+
+const minimum = 20
+const maximum = 50
+console.log(Math.floor((Math.random() * (maximum - minimum + 1)) + minimum))// return a random number between minimum and maximum (inclusive)
